6a352cb7d7fe55c58309c154e370feaa406e7dbb
Juddroid <nobllizdc@gmail.com>
2021-01-04 14:58:21 +0900

    HalfAdder

--- cs-10/cs-01.js
+++ cs-10/cs-01.js
@@ -1,9 +1,27 @@
+// Mission1: 이진 덧셈기
+
+
 function halfadder(bitA, bitB) {
-  var answer = [];
+  function sum(a, b) {
+    if ((!a && !b)||(a&b)) return false;
+    if ((!a && b)||(a&&!b)) return true;
+  }
+  
+  function carry(a, b) {
+    if(a && b) return true;
+    return false
+  }
+  
+  let answer = [carry(bitA, bitB), sum(bitA, bitB)];
   return answer;
 }
 
 function fulladder(bitA, bitB, carry) {
-  var answer = [];
+  let answer = [];
   return answer;
 }
+
+console.log(halfadder(false, false))
+console.log(halfadder(false, true))
+console.log(halfadder(true, false))
+console.log(halfadder(true, true))

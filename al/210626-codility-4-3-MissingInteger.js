3d7fdd6020e41cb319ef0e25018dfd7c3f59292b
Raccoon <nobllizdc@gmail.com>
2021-07-15 12:23:00 +0900

    [algorithm] codility 4-3 another mothod

--- al/210626-codility-4-3-MissingInteger.js
+++ al/210626-codility-4-3-MissingInteger.js
@@ -16,3 +16,23 @@ function solution(A) {
 
 let result = solution(A);
 console.log(result);
+
+// function solution(A) {
+//   let sorted = Array.from(new Set(A.sort((a, b) => a - b)));
+//   let sort = sorted.filter((el) => el > 0);
+
+//   if (sort.length === 0) return 1;
+//   if (sort.length === 1 && sort[0] !== 1) return 1;
+
+//   console.log(sort);
+
+//   let cur = sort[0];
+
+//   if (cur !== 1) return 1;
+
+//   for (let i = 1; i < sort.length; i++) {
+//     if (cur !== sort[i] - 1) return cur + 1;
+//     else cur = sort[i];
+//   }
+//   return sort.length + 1;
+// }

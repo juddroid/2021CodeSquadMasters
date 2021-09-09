4ac9d8d105844e3069dc4d7fc63bf87d6303bf0f
Raccoon <nobllizdc@gmail.com>
2021-09-10 04:53:19 +0900

    [algorithm] section01-10(fix)

--- al/inflearn/section01-10.js
+++ al/inflearn/section01-10.js
@@ -3,8 +3,10 @@ const word = 'COMPUTERPROGRAMMING';
 const char = 'R';
 
 function solution(word, char) {
-  let list = word.split('');
-  return list.filter((el) => el === char).length;
+  // let list = word.split('');
+  // return list.filter((el) => el === char).length;
+  console.log(word.split(char));
+  return word.split(char).length - 1;
 }
 
 console.log(solution(word, char));

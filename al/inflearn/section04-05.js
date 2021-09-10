0abe65daaec787917afc0bc44d0ab0c2bba9ae1e
Raccoon <nobllizdc@gmail.com>
2021-09-11 03:44:25 +0900

    [algorithm] section04-05(fix)

--- al/inflearn/section04-05.js
+++ al/inflearn/section04-05.js
@@ -8,17 +8,28 @@ let arr = [
 function solution(arr) {
   let first = arr.shift();
 
-  let temp = [];
+  // let temp = [];
+  // for (let i = 0; i < arr[0].length; i++) {
+  //   for (let j = 0; j < arr[0].length; j++) {
+  //     for (let k = 0; k < arr[0].length; k++) {
+  //       if (i === j || j === k || i === k) break;
+  //       temp.push(arr[0][i] + arr[0][j] + arr[0][k]);
+  //       temp.sort((a, b) => b - a);
+  //     }
+  //   }
+  // }
+  // let result = Array.from(new Set(temp));
+  // return result[2];
+
+  let temp = new Set();
   for (let i = 0; i < arr[0].length; i++) {
-    for (let j = 0; j < arr[0].length; j++) {
-      for (let k = 0; k < arr[0].length; k++) {
-        if (i === j || j === k || i === k) break;
-        temp.push(arr[0][i] + arr[0][j] + arr[0][k]);
-        temp.sort((a, b) => b - a);
+    for (let j = i + 1; j < arr[0].length; j++) {
+      for (let k = j + 1; k < arr[0].length; k++) {
+        temp.add(arr[0][i] + arr[0][j] + arr[0][k]);
       }
     }
   }
-  let result = Array.from(new Set(temp));
+  let result = Array.from(temp).sort((a, b) => b - a);
   return result[2];
 }
 

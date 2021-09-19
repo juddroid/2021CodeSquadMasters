a6431346f7132ce54a0c7513019d18847c947769
Raccoon <nobllizdc@gmail.com>
2021-09-19 23:51:18 +0900

    [algorithm] baekjoon

--- al/210919-baekjoon-3052.js
+++ al/210919-baekjoon-3052.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const input = fs.readFileSync('./test').toString().trim().split('\n');
 
-function test(input) {}
+function test(input) {
+  let dic = {};
+  let num = 42;
+  for (let i = 0; i < input.length; i++) {
+    let extra = +input[i] % num;
+    if (!dic[extra]) dic[extra] = true;
+  }
+  return Object.keys(dic).length;
+}
 
 console.log(test(input));
